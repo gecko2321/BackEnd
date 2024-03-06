77ff9cfa8c6cff27af7796f60ad4361645f4a375
rmoreno <gecko_2321@hotmail.com>
2024-03-06 19:41:36 -0300

    parte de users

---
+++ UserManager.js
@@ -0,0 +1,29 @@
+class UserManager {
+  static #users = [];
+  create(data) {
+    const user = {
+      id:
+        UserManager.#users.length === 0
+          ? 1
+          : UserManager.#users[UserManager.#users.lenght - 1].id + 1,
+      photo: data.photo,
+      email: data.email,
+      password: data.password,
+      role: 0,
+    };
+    UserManager.#users.push(user);
+    console.log("Usuario Creado");
+  }
+  read(){
+    return UserManager.#users
+  }
+}
+
+const gestorDeUsuarios = new UserManager()
+gestorDeUsuarios.create({
+    photo: "photo.png",
+    email: "gecko_2321@hotmail.com",
+    password: "12345678"
+ })
+//Crear un usuario mas
+console.log(gestorDeUsuarios.read())
